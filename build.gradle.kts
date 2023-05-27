902f5bf32bc88e073b56586137c0613f321c972f
Shkalin Vyacheslav <nigroborgin@yandex.ru>
2023-05-28 02:47:39 +0300

    add prject files

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    java
+    application
+    id("org.springframework.boot") version "2.7.11"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "ru.kpfu.itis.shkalin"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    // WEB
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-freemarker")
+
+    // DB
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("org.postgresql:postgresql")
+
+    // Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // Tests
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+}
+
+//apply(plugin = "application")
+application {
+    mainClass.set("ru.kpfu.itis.shkalin.spring_site_politics.App")
+}
+//mainClassName = "ru.kpfu.itis.shkalin.spring_site_politics.App"
